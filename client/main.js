e61497c9989f5e8f8726f8a5e664338a07244155
dq.tho89 <dq.tho89@gmail.com>
2017-11-02 21:53:01 +0700

    Tracking status login

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
@@ -9,6 +10,9 @@ import Link from './../imports/ui/Link';
 import NotFound from './../imports/ui/NotFound';
 import Login from './../imports/ui/Login';
 
+const unauthenticatedPage = ['/', '/signup'];
+const authenticatedPage = ['/links']
+
 const routes = (
   <Router history={browserHistory}>
     <Route path="/signup" component={SignUp}/>
@@ -17,6 +21,19 @@ const routes = (
     <Route path="/*" component={NotFound}/>
   </Router>
 );
+
+Tracker.autorun(() => {
+  isAuthenticated = !!Meteor.userId();
+  pathName = browserHistory.getCurrentLocation().pathname;
+  isUnauthenticatedPage = unauthenticatedPage.includes(pathName);
+  isAuthenticatedPage = authenticatedPage.includes(pathName);
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.push('/links');
+  } else if (isAuthenticatedPage && !isAuthenticated) {
+    browserHistory.push('/');
+  }
+})
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'))
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {browserHistory} from 'react-router';
+import {Accounts} from 'meteor/accounts-base';
 
 export default class Link extends React.Component {
   onLogOut() {
-    browserHistory.push('/')
+    Accounts.logout();
   }
 
   render() {
