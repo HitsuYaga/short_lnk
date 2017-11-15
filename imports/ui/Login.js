8c28d376ad0ca2845b18aff3bd79048061da5ca6
dq.tho89 <dq.tho89@gmail.com>
2017-11-15 17:00:58 +0700

    Add base styles using SCSS

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import {Meteor} from 'meteor/meteor';
-import {Link} from 'react-router';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { Link } from "react-router";
 
 export default class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      error: ''
+      error: ""
     };
   }
 
@@ -17,27 +17,34 @@ export default class Login extends React.Component {
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
 
-    Meteor.loginWithPassword({email}, password, (err) => {
+    Meteor.loginWithPassword({ email }, password, err => {
       if (err) {
-        this.setState(() => ({ error: err.reason }))
+        this.setState(() => ({ error: err.reason }));
       } else {
-        this.setState(() => ({ error: '' }))
+        this.setState(() => ({ error: "" }));
       }
-    })
+    });
   }
 
   render() {
     return (
-      <div>
-        <h1>Short Lnk</h1>
-        {this.state.error ? <p>{this.state.error}</p> : undefined}
-        <form onSubmit={this.onSubmit.bind(this)} noValidate>
-          <input ref="email" type="email" name="email" placeholder="Email"/>
-          <input ref="password" type="password" name="password" placeholder="Password"/>
-          <button>Login</button>
-        </form>
-        <Link to="/signup">Have an account?</Link>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Short Lnk</h1>
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
+          <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input ref="email" type="email" name="email" placeholder="Email" />
+            <input
+              ref="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <button>Login</button>
+          </form>
+          <Link to="/signup">Have an account?</Link>
+        </div>
       </div>
-    )
+    );
   }
 }
--- imports/ui/SignUp.js
+++ imports/ui/SignUp.js
@@ -34,20 +34,22 @@ export default class SignUp extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Join Short Lnk</h1>
-        {this.state.error ? <p>{this.state.error}</p> : undefined}
-        <form onSubmit={this.onSubmit.bind(this)} noValidate>
-          <input ref="email" type="email" name="email" placeholder="Email" />
-          <input
-            ref="password"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-          <button>Create Account</button>
-        </form>
-        <Link to="/">Already have an account?</Link>
+      <div className="box-view">
+        <div className="box-view__box">
+          <h1>Join Short Lnk</h1>
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
+          <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input ref="email" type="email" name="email" placeholder="Email" />
+            <input
+              ref="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <button>Create Account</button>
+          </form>
+          <Link to="/">Already have an account?</Link>
+        </div>
       </div>
     );
   }
