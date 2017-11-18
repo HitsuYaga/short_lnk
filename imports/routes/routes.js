e861561699f4aa01ccf91333e52e21b44c40f3aa
dq.tho89 <dq.tho89@gmail.com>
2017-11-18 12:27:50 +0700

    Change in routes

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -2,56 +2,39 @@ import { Meteor } from "meteor/meteor";
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
 
-import Link from "./../ui/Link";
-import SignUp from "./../ui/Signup";
-import NotFound from "./../ui/NotFound";
-import Login from "./../ui/Login";
+import Signup from "../ui/Signup";
+import Link from "../ui/Link";
+import NotFound from "../ui/NotFound";
+import Login from "../ui/Login";
 
-const unauthenticatedPage = ["/", "/signup"];
-const authenticatedPage = ["/links"];
-
+const unauthenticatedPages = ["/", "/signup"];
+const authenticatedPages = ["/links"];
 const onEnterPublicPage = () => {
   if (Meteor.userId()) {
-    browserHistory.push("/links");
+    browserHistory.replace("/links");
   }
 };
-
 const onEnterPrivatePage = () => {
   if (!Meteor.userId()) {
-    browserHistory.push("/");
+    browserHistory.replace("/");
   }
 };
-
 export const onAuthChange = isAuthenticated => {
-  pathName = browserHistory.getCurrentLocation().pathname;
-  isUnauthenticatedPage = unauthenticatedPage.includes(pathName);
-  isAuthenticatedPage = authenticatedPage.includes(pathName);
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
 
   if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.push("/links");
+    browserHistory.replace("/links");
   } else if (isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.push("/");
+    browserHistory.replace("/");
   }
 };
-
 export const routes = (
   <Router history={browserHistory}>
-    <Route path="/signup" component={SignUp} onEnter={onEnterPublicPage} />
     <Route path="/" component={Login} onEnter={onEnterPublicPage} />
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage} />
     <Route path="/links" component={Link} onEnter={onEnterPrivatePage} />
-    <Route path="/*" component={NotFound} />
+    <Route path="*" component={NotFound} />
   </Router>
 );
-
-Tracker.autorun(() => {
-  isAuthenticated = !!Meteor.userId();
-  pathName = browserHistory.getCurrentLocation().pathname;
-  isUnauthenticatedPage = unauthenticatedPage.includes(pathName);
-  isAuthenticatedPage = authenticatedPage.includes(pathName);
-
-  if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.push("/links");
-  } else if (isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.push("/");
-  }
-});
